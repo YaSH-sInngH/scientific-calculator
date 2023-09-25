e43b5b0617b22ed70f75ea9e4d9880d4b7a4cc92
Yash <singhyash@0026gmail.com>
2023-09-25 17:50:14 +0530

    committed

---
+++ app.js
@@ -0,0 +1,51 @@
+let string='';
+let buttons = document.querySelectorAll('.container-grid button');
+let pendingSquareRoot = false;
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        
+        if(e.target.innerHTML == 'Enter'){
+            string = eval(string);
+            document.querySelector('.js-input').value = string;
+        }
+        else if(e.target.innerHTML == 'C'){
+            string = '';
+            document.querySelector('.js-input').value = string;
+        }
+        else if(e.target.innerHTML == 'Del'){
+            string = string.slice(0, -1);
+            document.querySelector('.js-input').value = string;
+        }
+        else if(e.target.innerHTML == 'π'){
+            pi = Math.PI;
+            string = string + eval(pi);
+            document.querySelector('.js-input').value = string;
+        }
+        else if(e.target.innerHTML == 'sin'){
+            const radians = (parseFloat(string) * Math.PI) / 180;
+            string = Math.sin(radians);
+            document.querySelector('.js-input').value = string;
+        }
+        else if(e.target.innerHTML == 'cos'){
+            const radians = (parseFloat(string) * Math.PI) / 180;
+            string = Math.cos(radians);
+            document.querySelector('.js-input').value = string;
+        }
+        else if(e.target.innerHTML == 'tan'){
+            const radians = (parseFloat(string) * Math.PI) / 180;
+            string = Math.tan(radians);
+            document.querySelector('.js-input').value = string;
+        }
+        else if (e.target.innerHTML == '√')
+        {
+            string = Math.sqrt(string);
+            document.querySelector('.js-input').value = string;
+        }
+        else{
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector('.js-input').value = string;
+        }
+    });
+});
